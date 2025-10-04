a613f6bef79a34f7818793cf58d58faf0518968b
Jamalious <johnynana12@gmail.com>
2025-10-03 22:13:51 -0600

    Adding comments

--- Interactive-Scene-John/sketch.js
+++ Interactive-Scene-John/sketch.js
@@ -3,35 +3,34 @@
 // 9/26/2025
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// - used mouseWheel to toggle the color of lines and shape outlines 
+// 
 
 let gameState = 'kaleidoscope';
-let button_1 = false;
-let x = 400;
-let y = 400;
-let lastSwitchedTime = 0;
-lineDuration = 4000;
 let reflectX = false;
 let reflectY = false;
 let shape_type = "line";
 let x_1, y_1;
 let x_2, y_2;
+let colorHue = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
-  colorMode(HSB);
+  colorMode(HSB, 360, 100, 100);
   background (255, 128, 64);
   strokeWeight(2);
   noStroke();
   noFill();
 }
 function draw() {
-  // create_shapes();
-  // line_setup();
-  // next_lines();
+  if (mouseIsPressed) {
+    let a = color(colorHue, 100, 100);
+    stroke(a);
+    noFill();
+  }
+
   if (shape_type === "line" && mouseIsPressed) {
-    stroke(0);
     if ( x_2 !== undefined && y_2 !== undefined) {
       symmetric_lines(x_2, y_2, mouseX, mouseY);
     }
@@ -43,6 +42,7 @@ function mousePressed(){
   x_1 = mouseX;
   y_1 = mouseY;
 }
+
 function mouseReleased(){
   if (shape_type === "circle") {
     symmetric_circles(x_1, y_1, mouseX, mouseY);
@@ -50,11 +50,12 @@ function mouseReleased(){
   else if (shape_type === "rectangle") {
     symmetric_rect(x_1, y_1, mouseX, mouseY);
   }
+  // reseting previous mouse position values after each drag and click down.
   x_2 = undefined;
   y_2 = undefined;
 }
 
-//Reflecting lines
+// symmetry functions for reflecting shapes across x, y, and x-y axis.
 function symmetric_lines(x1, y1, x2, y2) {
   line(x1, y1, x2, y2);
   if (reflectX) {
@@ -99,6 +100,7 @@ function symmetric_rect(x1,y1,x2,y2) {
   
 }
 function keyPressed() {
+  //using keys to toggle the reflction axes and type of shape to draw
   if (key === 'x'){
     reflectX = ! reflectX;
   }
@@ -118,27 +120,31 @@ function keyPressed() {
     shape_type = "rectangle";
   }
 }
-
-function create_shapes(){
-  if (gameState === 'kaleidoscope') {
-    translate( width /2 , height / 2) ;
-  }
-  if (gameState === 'kaleidoscope') {
-    let shape_segments = 6;
-    let rotation_angle = 360 / shape_segments;
-    for ( let shape = 0; shape < shape_segments; shape++){
-      fill( 45* shape , 45, 175);
-      rotate(rotation_angle);
-      stroke(150);
-      strokeWeight(sw_selector);
-      line(x_1, y_1, x_2, y_2);
-      // reflecting the line
-      scale( 1.1, -1.1);
-      line(x_1, y_1, x_2, y_2);
-      pop();
-    }
-  }
+function mouseWheel(event) {
+  colorHue += event.delta /5; //setting sensitivity
+  colorHue = (colorHue + 360) % 360; // range from 0-360;
 }
 
-
+function kaleidoscope_effect(){
+  if (gameState === "kaleidoscope") {
+    translate(width / 2, height /2);
+    if (shape_type === "line" && mouseIsPressed) {
+      let x1 = mouseX - width /2;
+      let y1 = mouseY - height /2;
+      let x2 = pmouseX - width /2;
+      let y2 = pmouseY - height /2;
+      let s_rotation = 360 / segments;
+      for (let segments = 0; segments < shape_segments; segments++) {
+        push();
+        rotate(s_rotation * segments);
+        stroke(0);
+        line(x1, y1, x2, y2);
+        //reflecting about the origin
+        scale(1, -1);
+        line(x1, y1, x2, y2);
+        pop();
+      }
+    }
+  } 
+}
 
