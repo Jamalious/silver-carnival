cd70b8ca1d5b0dc3760698fa74a1a9e8952416a1
Jamalious <johnynana12@gmail.com>
2025-10-15 09:55:16 -0600

    Implementing math evaluation function
    Testing how to make create random math questions and check if the user corrects the correct answer

--- arrays-objects/sketch.js
+++ arrays-objects/sketch.js
@@ -5,17 +5,23 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let pos = (x, y);
+let pos = {
+  x: 0, 
+  y: 0,
+};
+let number1;
+let number2;
+let solution;
+
 
 function preload() {
   partyConnect(
 
     
-  )
+  );
+  pos = partyLoadShared("pos", pos);
 
 }
-
-
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -23,8 +29,16 @@ function setup() {
 
 function draw() {
   background(220);
+  ellipse(pos.x, pos.y, 100, 100);
+  
+}
+
+function check_addition(a, b, Sum) {
+  return a + b === Sum;
+  
 }
 
 function mousePressed(){
-
+  pos.x = mouseX;
+  pos.y = mouseY;
 }
