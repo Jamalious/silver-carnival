ee3cd4e78b52df3426eb4315b70f5a6daf6ed49c
Jamalious <johnynana12@gmail.com>
2025-10-21 09:54:07 -0600

    Exploring Classes
    Implementing classes to initalize and control the movement of player cars during the session

--- arrays-objects/sketch.js
+++ arrays-objects/sketch.js
@@ -1,4 +1,4 @@
-// Typing game 
+// Multiplayer Typing game 
 // John Asiamah
 // Date
 //
@@ -6,8 +6,9 @@
 // - describe what you did to take this project "above and beyond"
 
 const words = ['might', 'much', 'where', 'why', 'but', 'however', 'without', 'cause', 'how', 'such', 'that', 'into' ];
-
+const displayElement = document.getElementById("randomWordDisplay");
 let botLoadTime = 3000;
+let button = 'false';
 let number1;
 let number2;
 let solution;
@@ -16,29 +17,72 @@ let typeLog = [];
 let botSpawnTime = 2000;
 let lastSpawned = 0;
 
+class Cars {
+  constructor(x, y, carWidth, carHeight, dx) {
+    this.x = x;
+    this.y = y;
+    this.carWidth = carWidth;
+    this.carHeight = carHeight;
+    this.dx = dx;
+  }
+}
+
 //Characteristics of each players car
 let playerOneCar = {
-  x: 0, 
-  y: 0,
+  x: 40, 
+  y: 100,
   vel: 0,
   skip_word: 0,
   avg_accuracy: 0,
   session: 0,
+  isSpawned: false,
 
 };
 let playerTwoCar = {
-  x2: 0,
-  y2: 0,
+  x2: 40,
+  y2: 300,
   vel2: 0,
   skip_word: 0,
   avg_accuracy: 0, 
   session: 0,
+  isSpawned: false,
 };
+
+let playerThreeCar = {
+  x: 40, 
+  y: 100,
+  vel: 0,
+  skip_word: 0,
+  avg_accuracy: 0,
+  session: 0,
+  isSpawned: false,
+
+};
+let playerFourCar = {
+  x2: 40,
+  y2: 300,
+  vel2: 0,
+  skip_word: 0,
+  avg_accuracy: 0, 
+  session: 0,
+  isSpawned: false,
+};
+let playerFiveCar = {
+  x: 40, 
+  y: 100,
+  vel: 0,
+  skip_word: 0,
+  avg_accuracy: 0,
+  session: 0,
+  isSpawned: false,
+};
+
 let bots = {
-  x3: 0,
-  y3: 0,
+  x: 40,
+  y: 100,
   vel3: 0,
   accuracy: 0,
+  isSpawned: false,
 };
 
 function preload() {
@@ -50,6 +94,7 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  partyToggleInfo(true);
   background(0);
 }
 
@@ -57,44 +102,36 @@ function setup() {
 function draw() {
   background(220);
   // Pretend these circles are cars for now
-  ellipse(playerOneCar.x, playerTwoCar.y, 100, 100);
-  ellipse(playerTwoCar.x1, playerTwoCar.y1, 100, 100);
-  ellipse(bots.x1, bots.x2, bots.x3, 100, 100);
+  ellipse(playerOneCar.x, playerOneCar.y, 100, 100);
+  ellipse(playerTwoCar.x2, playerTwoCar.y2, 100, 100);
+  ellipse(bots.x3, bots.y3, 100, 100);
   start_session();
-  generate_words();
-  
 }
-
 
 function mousePressed(){
   playerOneCar.x = mouseX;
-  playerOneCar.y = mouseY;
 }
 
 function start_session() {
-  if (button_2.mousePressed){
+  if (button.mousePressed){
     generate_words();
   }
 }
 function generate_words(){
   const randomIndex = Math.floor (random() * words.length);
-  const displayTheWords = words(randomIndex);
   for (let i = 0; i < 200; i ++){
     currentIndex = words[randomIndex]; 
   }
-  //how to display these words on the screen? work on style/html stuff later
-  console.log(currentIndex);
-}
-// creating a keylogger
-function keylogger() {
   for (character of currentIndex){
     if (key === currentIndex[character]){
+      playerOneCar.x =+ 0.001;
     }
   }
-}
-new_bot() {
+  //how to display these words on the screen? work on style/html stuff later
+  console.log(currentIndex);
 
 }
+
 function moveCarsAcrossScreen(){
 
 }
