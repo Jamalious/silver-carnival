9fe2bfb6650d7e5b6bfc38957a487db84d8965f6
Jamalious <johnynana12@gmail.com>
2025-10-21 20:51:49 -0600

    debugging

--- arrays-objects/sketch.js
+++ arrays-objects/sketch.js
@@ -6,7 +6,6 @@
 // - describe what you did to take this project "above and beyond"
 
 const words = ['might', 'much', 'where', 'why', 'but', 'however', 'without', 'cause', 'how', 'such', 'that', 'into' ];
-const displayElement = document.getElementById("randomWordDisplay");
 let botLoadTime = 3000;
 let button = 'false';
 let number1;
@@ -14,7 +13,7 @@ let number2;
 let solution;
 let correctKey = false;
 let typeLog = [];
-let botSpawnTime = 2000;
+let startSession = 'false';
 let lastSpawned = 0;
 
 class Cars {
@@ -23,8 +22,32 @@ class Cars {
     this.y = y;
     this.carWidth = carWidth;
     this.carHeight = carHeight;
-    this.dx = dx;
+    this.dx = 0;
+    this.inSession = false;
+    this.id = random(1, 10);
   }
+  moveCar() {
+    if (this.inSession) { 
+    }
+  }
+}
+class theBots {
+  constructor(x, y, carWidth, carHeight, dx) {
+    this.x = x;
+    this.y = y;
+    this.carWidth = carWidth;
+    this.carHeight = carHeight;
+    this.dx = 0;
+    this.inSession = false;
+    this.id = random(1, 10);
+  }
+  moveCar() {
+    if (this.inSession) { 
+    }
+
+
+  }
+
 }
 
 //Characteristics of each players car
@@ -48,35 +71,6 @@ let playerTwoCar = {
   isSpawned: false,
 };
 
-let playerThreeCar = {
-  x: 40, 
-  y: 100,
-  vel: 0,
-  skip_word: 0,
-  avg_accuracy: 0,
-  session: 0,
-  isSpawned: false,
-
-};
-let playerFourCar = {
-  x2: 40,
-  y2: 300,
-  vel2: 0,
-  skip_word: 0,
-  avg_accuracy: 0, 
-  session: 0,
-  isSpawned: false,
-};
-let playerFiveCar = {
-  x: 40, 
-  y: 100,
-  vel: 0,
-  skip_word: 0,
-  avg_accuracy: 0,
-  session: 0,
-  isSpawned: false,
-};
-
 let bots = {
   x: 40,
   y: 100,
@@ -96,6 +90,8 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   partyToggleInfo(true);
   background(0);
+  gameSession();
+  generateWords();
 }
 
 
@@ -105,30 +101,31 @@ function draw() {
   ellipse(playerOneCar.x, playerOneCar.y, 100, 100);
   ellipse(playerTwoCar.x2, playerTwoCar.y2, 100, 100);
   ellipse(bots.x3, bots.y3, 100, 100);
-  start_session();
 }
 
 function mousePressed(){
   playerOneCar.x = mouseX;
 }
 
-function start_session() {
-  if (button.mousePressed){
-    generate_words();
-  }
+
+function generateWords(){
+  const randomIndex = Math.floor(random() * words.length);
+  return words[randomIndex];
 }
-function generate_words(){
-  const randomIndex = Math.floor (random() * words.length);
-  for (let i = 0; i < 200; i ++){
-    currentIndex = words[randomIndex]; 
-  }
-  for (character of currentIndex){
-    if (key === currentIndex[character]){
-      playerOneCar.x =+ 0.001;
+function gameSession(){
+  if (startSession){
+    document.getElementById('words').innerHTML =  '';
+    for (let i = 0; i < 200; i ++){
+      document.getElementById('words').innerHTML += generateWords();
     }
+    for (character of currentIndex){
+      if (key === currentIndex[character]){
+        playerOneCar.x =+ 0.001;
+      }
+    }
+    //how to display these words on the screen? work on style/html stuff later
+    console.log(currentIndex);
   }
-  //how to display these words on the screen? work on style/html stuff later
-  console.log(currentIndex);
 
 }
 
